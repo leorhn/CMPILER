d9275f976141078cf558de5ad5810adc196af619
Benson Polican <benson_polican@dlsu.edu.ph>
2017-10-08 23:28:16 +0800

    part of ui and other dependencies

---
+++ Friendly/Friendly Beta/src/view/MainScreen.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.*;
+
+public class MainScreen {
+    private JTextArea txtAreaInput;
+    private JButton btnSend;
+    private JTextPane txtPaneResult;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
